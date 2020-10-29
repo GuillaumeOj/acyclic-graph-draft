a1966f1f34c823d6f7bab9f3bdee50d3c688e193
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-29 12:54:27 +0100

    Add Referral

--- main.py
+++ main.py
@@ -6,26 +6,40 @@ class RefereeIsAlreadyReferred(BaseException):
     pass
 
 
-class User:
+class Referral:
+    referrals = list()
+
     def __init__(self):
         self.referrer = None
+        self.referee = None
 
-    def set_referrer(self, referrer):
-        if self.referrer:
-            raise RefereeIsAlreadyReferred
-
+    def set_referral(self, referee, referrer):
         try:
-            self.check_referrer(referrer)
-        except CircularRefer as e:
+            self.check_referral(referee, referrer)
+        except Exception as e:
             raise e
 
+        self.referee = referee
         self.referrer = referrer
+        Referral.referrals.append(self)
 
-    def check_referrer(self, referrer):
-        previous_referrer = referrer.referrer
+    def check_referral(self, referee, referrer):
+        if self.get_referrer(referee):
+            raise RefereeIsAlreadyReferred
 
+        previous_referrer = self.get_referrer(referrer)
         while previous_referrer:
-            if previous_referrer == self:
+            if previous_referrer == referee:
                 raise CircularRefer
+            if previous_referrer:
+                previous_referrer = self.get_referrer(previous_referrer)
 
-            previous_referrer = previous_referrer.referrer
+    def get_referrer(self, referee):
+        for referral in Referral.referrals:
+            if referral.referee == referee:
+                return referral.referrer
+
+
+class User:
+    def __init__(self, id=None):
+        self.id = id
--- test_main.py
+++ test_main.py
@@ -3,93 +3,104 @@ import pytest
 from main import RefereeIsAlreadyReferred
 from main import CircularRefer
 from main import User
+from main import Referral
 
 
-def test_user():
-    A = User()
-    assert A.referrer is None
+@pytest.fixture
+def referrals():
+    Referral.referrals = list()
+    yield
 
 
-def test_referrer_has_referees():
-    A = User()
-    B = User()
-
-    B.set_referrer(A)
-
-    assert A.referrer is None
-
-    assert B.referrer == A
-
-
-def test_user_refer_a_referrer_with_referees():
+def test_referrals(referrals):
     A = User()
     B = User()
-    C = User()
 
-    B.set_referrer(A)
-    A.set_referrer(C)
+    Referral().set_referral(referee=B, referrer=A)
 
-    assert A.referrer == C
-    assert B.referrer == A
-    assert C.referrer is None
+    assert Referral.referrals[0].referee == B
+    assert Referral.referrals[0].referrer == A
+    assert len(Referral.referrals) == 1
 
 
-def test_user_refer_a_user_with_referrer():
+def test_user_refer_a_referrer_with_referees(referrals):
     A = User()
     B = User()
     C = User()
 
-    B.set_referrer(A)
+    Referral().set_referral(referee=B, referrer=A)
+    Referral().set_referral(referee=A, referrer=C)
+
+    assert len(Referral.referrals) == 2
+
+    assert Referral.referrals[0].referee == B
+    assert Referral.referrals[0].referrer == A
+
+    assert Referral.referrals[1].referee == A
+    assert Referral.referrals[1].referrer == C
+
+
+def test_user_refer_a_user_with_referrer(referrals):
+    A = User()
+    B = User()
+    C = User()
+
+    Referral().set_referral(referee=B, referrer=A)
 
     with pytest.raises(RefereeIsAlreadyReferred):
-        B.set_referrer(C)
+        Referral().set_referral(referee=B, referrer=C)
 
-    assert A.referrer is None
+    assert len(Referral.referrals) == 1
 
-    assert B.referrer == A
+    assert Referral.referrals[0].referee == B
+    assert Referral.referrals[0].referrer == A
 
-    assert C.referrer is None
 
-
-def test_user_refer_his_referrer():
+def test_user_refer_his_referrer(referrals):
     A = User()
     B = User()
 
-    B.set_referrer(A)
+    Referral().set_referral(referee=B, referrer=A)
 
     with pytest.raises(CircularRefer):
-        A.set_referrer(B)
+        Referral().set_referral(referee=A, referrer=B)
 
-    assert A.referrer is None
-    assert B.referrer == A
+    assert len(Referral.referrals) == 1
+
+    assert Referral.referrals[0].referee == B
+    assert Referral.referrals[0].referrer == A
 
 
-def test_circular_refer():
-    # more than 3 levels
-    A = User()
-    B = User()
-    C = User()
-    D = User()
+def test_circular_refer_with_four_levels(referrals):
+    A = User("A")
+    B = User("B")
+    C = User("C")
+    D = User("D")
 
-    B.set_referrer(A)
-    C.set_referrer(B)
-    D.set_referrer(C)
+    users = [A, B, C, D]
+
+    Referral().set_referral(referee=B, referrer=A)
+    Referral().set_referral(referee=C, referrer=B)
+    Referral().set_referral(referee=D, referrer=C)
 
     with pytest.raises(CircularRefer):
-        A.set_referrer(D)
+        Referral().set_referral(referee=A, referrer=D)
 
-    assert A.referrer is None
+    assert len(Referral.referrals) == len(users) - 1
 
-    assert B.referrer == A
+    for i, referral in enumerate(Referral.referrals):
+        assert referral.referee == users[i + 1]
+        assert referral.referrer == users[i]
 
-    assert C.referrer == B
 
-    assert D.referrer == C
-
-    # many levels
+def test_circular_refer_with_thousand_levels(referrals):
     users = [User() for _ in range(1000)]
 
-    [users[i + 1].set_referrer(users[i]) for i in range(len(users)) if i < len(users) - 1]
+    [
+        Referral().set_referral(referee=users[i + 1], referrer=users[i])
+        for i in range(len(users))
+        if i < len(users) - 1
+    ]
 
     with pytest.raises(CircularRefer):
-        users[0].set_referrer(users[-1])
+        Referral().set_referral(referee=users[0], referrer=users[-1])
